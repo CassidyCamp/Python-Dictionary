71d39feaef61e3868c32d8b0e90fff60f868654a
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2025-09-30 08:47:38 +0500

    done

--- task30.py
+++ task30.py
@@ -2,7 +2,7 @@ def filter_non_zero(d: dict[str, int]) -> dict[str, int]:
     result = {}
 
     for key, value in d.items():
-        if value == 0:result.update({key: value})
+        if value != 0:result.update({key: value})
     return result
 
 dictt = {"a": 0, "b": 5, "c": -3}
